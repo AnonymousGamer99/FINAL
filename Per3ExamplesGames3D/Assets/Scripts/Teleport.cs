f846ab0c3cdac0d67ff4cb8978e3743adfa35c24
Gamerdude12321 <tomaskaplaniscoll@gmail.com>
2016-05-03 13:12:42 -0700

    Added TP Script

---
+++ Per3ExamplesGames3D/Assets/Scripts/Teleport.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+	public GameObject player;
+	public float x;
+	public float y;
+	public float z;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other){
+		player.transform.position = new Vector3(x,y,z);
+	}
+}
