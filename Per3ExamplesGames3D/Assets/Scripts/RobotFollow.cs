065fd803e99e620fc15c5177dc856723e2ecc391
Gamerdude12321 <tomaskaplaniscoll@gmail.com>
2016-05-12 12:24:26 -0700

    The robot is SOOOO CREEPY OMG WAT

---
+++ Per3ExamplesGames3D/Assets/Scripts/RobotFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotFollow : MonoBehaviour {
+
+	//You may consider adding a rigid body to the robot for accurate physics simulation
+	private GameObject wayPoint;
+	private Vector3 wayPointPos;
+	public Transform FPSController;
+	//This will be the robot's speed. Adjust as necessary.
+	private float speed = 6.0f;
+	void Start ()
+	{
+		//At the start of the game, the zombies will find the gameobject called wayPoint.
+		wayPoint = GameObject.Find("wayPoint");
+	}
+	
+	void Update ()
+	{
+		wayPointPos = new Vector3(wayPoint.transform.position.x, wayPoint.transform.position.y, wayPoint.transform.position.z);
+		//Here, the zombie's will follow the waypoint.
+		transform.position = Vector3.MoveTowards(transform.position, wayPointPos, speed * Time.deltaTime);
+		transform.LookAt (FPSController);
+	}
+}
